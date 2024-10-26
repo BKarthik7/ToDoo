b0d62cb5df2d82b13d15e2a13fb1f6f3e1fc3ee6
B Karthik <bangikarthik7@gmail.com>
2024-10-26 17:09:43 +0530

    aakhri

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,16 +12,16 @@ export default function App() {
 
   return (
     <div className="App">
-      {/* {auth ? ( */}
+      {auth ? (
         <>
-          {/* <button onClick={handleLogout} className="absolute top-4 right-4 bg-red-500 text-white px-4 py-2 rounded">
+          <button onClick={handleLogout} className="absolute top-4 right-4 bg-red-500 text-white px-4 py-2 rounded">
             Logout
-          </button> */}
+          </button>
           <TodoList />
         </>
-      {/* ) : (
+      ) : (
         <Auth setAuth={setAuth} />
-      )} */}
+      )}
     </div>
   );
 }
